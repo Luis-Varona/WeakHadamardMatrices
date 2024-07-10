20a0c80ea7ea3ec4a1851c36a36d92a6f98dc7fa
Luis Miguel Varona <lbvarona@mta.ca>
2024-07-10 04:04:22 -0300

    Save order lists to file as generated and add (incomplete) .mat file

--- main.m
+++ main.m
@@ -2,8 +2,10 @@ addpath(genpath('isequaltol.m'))
 addpath(genpath('npermutek'))
 
 WeakHadamardMatrices_3 = getWeakHadamards(3);
-WeakHadamardMatrices_4 = getWeakHadamards(4);
-WeakHadamardMatrices_5 = getWeakHadamards(5);
+save('WeakHadamardMatrices_3to5.mat', 'WeakHadamardMatrices_3')
 
-save('WeakHadamardMatrices_3to5.mat', 'WeakHadamardMatrices_3', ...
-    'WeakHadamardMatrices_4', 'WeakHadamardMatrices_5')
+WeakHadamardMatrices_4 = getWeakHadamards(4);
+save('WeakHadamardMatrices_3to5.mat', 'WeakHadamardMatrices_4', '-append')
+
+WeakHadamardMatrices_5 = getWeakHadamards(5);
+save('WeakHadamardMatrices_3to5.mat', 'WeakHadamardMatrices_5', '-append')
